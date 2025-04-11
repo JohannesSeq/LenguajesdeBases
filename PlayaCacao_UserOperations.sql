8a9e3126462216c15b8710dcf259dd7c05110fc4
Sequeira Fernandez Johannes Armando <84215987+JohannesSeq@users.noreply.github.com>
2025-04-10 23:51:51 -0600

    Validando procedimientos

--- PlayaCacao_UserOperations.sql
+++ PlayaCacao_UserOperations.sql
@@ -51,6 +51,26 @@ EXEC CREAR_PERSONA(1200345, 'Nidia', 'Infante', '123-456','Cliente', 1, 21, 23, 
 
 --Creacion de puestos
 EXEC CREAR_PUESTO('Mesero', 300000, 'Mesero del restaurante', 'Agregando puesto');
+EXEC CREAR_PUESTO('Cajero', 600000, 'Cajero del restaurante', 'Agregando puesto');
+EXEC CREAR_PUESTO('Gerente', 900000, 'Gerente general del restaurante', 'Agregando puesto');
 
 --Creacion de departamentos
 EXEC CREAR_DEPARTAMENTO('Gerenncia','Departamento administrativo', 'Agregando Departamento');
+EXEC CREAR_DEPARTAMENTO('Saloneria','Departamento de salon', 'Agregando Departamento');
+EXEC CREAR_DEPARTAMENTO('Cocina','Departamento de cocina', 'Agregando Departamento');
+
+--Creacion de empleados
+EXEC CREAR_EMPLEADO(10001,1,22,1000000,1184401,'Agregando Empleado');
+
+--Agregar platillos
+EXEC CREAR_PLATILLO('Arroz con camarones', 5000, 50, 'Agregando platillos');
+EXEC CREAR_PLATILLO('Papas fritas', 2000, 100, 'Agregando platillos');
+EXEC CREAR_PLATILLO('Dedos de pollo', 3000, 80, 'Agregando platillos');
+
+
+--Agregar menu
+EXEC CREAR_MENU('Menu Familiar','Menu con todos los platillos para la familia', 'Agregando menus');
+
+--Agregar platillos al menu
+EXEC CREAR_PLATILLO_MENU(1,1,'Agregando platillos al menu');
+EXEC CREAR_PLATILLO_MENU(2,1,'Agregando platillos al menu');
