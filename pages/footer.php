1cc2c3a9ee39bb975c5b3159c69f4cbd359b91c2
Daraya29 <42704650+Jotadeiru@users.noreply.github.com>
2025-04-15 20:21:16 -0600

    Customizando footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,52 +1,58 @@
+<!-- Font Awesome CDN -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-aEtUUnHNeH44U0ibnCDEY8EoVoUIutV1kzMX7Mk9ihpO1D8k5T0yA5UjL+yJ/hC9p2VgBoN2kF4mN4qT2sqo1g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h6>About</h6>
-            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
-          </div>
-
-          <div class="col-xs-6 col-md-3">
-            <h6>Categories</h6>
-            <ul class="footer-links">
-              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
-            </ul>
-          </div>
-
-          <div class="col-xs-6 col-md-3">
-            <h6>Quick Links</h6>
-            <ul class="footer-links">
-              <li><a href="http://scanfcode.com/about/">About Us</a></li>
-              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
-            </ul>
-          </div>
-        </div>
-        <hr>
+  <div class="container">
+    <div class="row">
+      <?php if (isset($_COOKIE["permiso"])): ?>
+      <div class="col-sm-12 col-md-6">
+        <h6>Sobre el sistema</h6>
+        <p class="text-justify">Este sistema fue diseñado para facilitar la gestión del restaurante Playa Cacao, permitiendo a los usuarios hacer pedidos, visualizar menús y a los empleados gestionar el negocio eficientemente.</p>
       </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
-         <a href="#">Scanfcode</a>.
-            </p>
-          </div>
 
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
-            </ul>
-          </div>
-        </div>
+      <div class="col-xs-6 col-md-3">
+        <h6>Enlaces útiles</h6>
+        <ul class="footer-links">
+          <li><a href="index.php">Inicio</a></li>
+          <?php if ($_COOKIE["permiso"] == "Limitado"): ?>
+            <li><a href="mostrarMenu.php">Ver Menú</a></li>
+            <li><a href="agregarPedido.php">Hacer Pedido</a></li>
+          <?php elseif ($_COOKIE["permiso"] == "Parcial" || $_COOKIE["permiso"] == "Completo"): ?>
+            <li><a href="listadoFacturas.php">Facturas y Pedidos</a></li>
+            <li><a href="mostrarPlatillos.php">Inventario</a></li>
+          <?php endif; ?>
+
+          <?php if ($_COOKIE["permiso"] == "Completo"): ?>
+            <li><a href="mostrarPersonas.php">Gestión de Personal</a></li>
+            <li><a href="agregarEmpleado.php">Agregar Empleado</a></li>
+          <?php endif; ?>
+        </ul>
       </div>
+      <?php endif; ?>
+
+      <div class="col-xs-6 col-md-3">
+        <h6>Redes Sociales</h6>
+        <ul class="footer-links">
+        <li><a class="facebook" href="https://www.facebook.com/PlayaCacao" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a class="instagram" href="https://www.instagram.com/playacacao_beach/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+    <hr>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-sm-6 col-xs-12">
+        <p class="copyright-text">© 2025 Playa Cacao Restaurant. Todos los derechos reservados.</p>
+      </div>
+
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <ul class="social-icons">
+        <li><a class="facebook" href="https://www.facebook.com/PlayaCacao" target="_blank"><i class="fa fa-facebook-f"></i></a></li>
+        <li><a class="instagram" href="https://www.instagram.com/playacacao_beach/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </footer>
+
